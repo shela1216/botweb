f6ce0a93b094998801554f58b432b1208327718a
shela1216 <shela1216@gmail.com>
2022-08-12 21:47:28 +0800

    no message

---
+++ js/about.bec9698c.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkgroupweb"]=self["webpackChunkgroupweb"]||[]).push([[443],{1272:function(u,n,t){t.r(n),t.d(n,{default:function(){return i}});var e=function(){var u=this;u._self._c;return u._m(0)},s=[function(){var u=this,n=u._self._c;return n("div",{staticClass:"about"},[n("h1",[u._v("This is an about page")])])}],r=t(1001),a={},l=(0,r.Z)(a,e,s,!1,null,null,null),i=l.exports}}]);
+//# sourceMappingURL=about.bec9698c.js.map
